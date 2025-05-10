871f14f6f942c811ccb78b44a54e79424e914677
Vasilena Tsacheva <73429767+vstchv@users.noreply.github.com>
2025-05-10 23:47:07 +0300

    Resp 200 OK

--- app/main.js
+++ app/main.js
@@ -1,12 +1,14 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Listening on port 4221");
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on("data", (data) => {
+    const resp = "HTTP/1.1 200 OK\r\n\r\n";
+    socket.write(resp);
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "::"); // binds to IPv6 and may accept IPv4-mapped addresses
