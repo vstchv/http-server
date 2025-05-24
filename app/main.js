0049d2d6bbd9a675b22147e9c96edbefb882577f
Vasilena Tsacheva <73429767+vstchv@users.noreply.github.com>
2025-05-24 19:51:26 +0300

    Respond with body when GET /echo/

--- app/main.js
+++ app/main.js
@@ -11,15 +11,26 @@ const server = net.createServer((socket) => {
     const [method, path, httpVersion] = requestLine.split(" ");
 
     let response;
-    if (method === "GET" && path === "/") {
-      response = "HTTP/1.1 200 OK\r\n\r\n";
+    if (method === "GET") {
+      if (path === "/") response = "HTTP/1.1 200 OK\r\n\r\n";
+      else if (path.startsWith("/echo/")) {
+        const echoString = path.slice("/echo/".length);
+        const contentLength = Buffer.byteLength(echoString, "utf-8");
+        response =
+          `HTTP/1.1 200 OK\r\n` +
+          `Content-Type: text/plain\r\n` +
+          `Content-Length: ${contentLength}\r\n\r\n` +
+          echoString;
+      } else {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
     } else {
       response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
 
     console.log(response);
 
-    socket.write(data);
+    socket.write(response);
     socket.end();
   });
 });
